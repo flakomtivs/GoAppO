6db6b9152f3fb745f6c0bb39c67e8e7c852accdd
flakomtivs <64364043+flakomtivs@users.noreply.github.com>
2021-11-14 15:13:57 +0100

    update 1.1!

---
+++ app/src/main/java/go/app/newe/settings/About_App.java
@@ -0,0 +1,16 @@
+package go.app.newe.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import go.app.newe.R;
+
+public class About_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+    }
+}
---
+++ app/src/main/java/go/app/newe/settings/My_Profil.java
@@ -0,0 +1,71 @@
+package go.app.newe.settings;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import go.app.newe.R;
+import go.app.newe.login.Login;
+
+public class My_Profil extends AppCompatActivity {
+    TextView nameTV;
+    GoogleSignInClient mGoogleSignInClient;
+    TextView emailTV;
+    TextView idTV;
+    ImageView photoIV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profil);
+
+
+        nameTV = findViewById(R.id.name);
+        emailTV = findViewById(R.id.email);
+        idTV = findViewById(R.id.id);
+        photoIV = findViewById(R.id.photo);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(My_Profil.this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            nameTV.setText("Name: "+personName);
+            emailTV.setText("Email: "+personEmail);
+            idTV.setText("ID: "+personId);
+            Glide.with(this).load(personPhoto).into(photoIV);
+        }
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(this, Settings_Activity.class);
+        startActivity(intent);
+    }
+}
